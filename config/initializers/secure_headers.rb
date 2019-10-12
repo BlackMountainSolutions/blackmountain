b812647448df055544b5206c1d7a3f11d333a221
Franco Montenegro <franco.montenegro.ruke@gmail.com>
2019-10-12 04:18:45 -0300

    More improvements in secure headers

--- config/initializers/secure_headers.rb
+++ config/initializers/secure_headers.rb
@@ -1,14 +1,15 @@
 SecureHeaders::Configuration.default do |config|
-    config.hsts = "max-age=#{1.week.to_i}"
     config.x_frame_options = "DENY"
     config.x_content_type_options = "nosniff"
     config.x_xss_protection = "1; mode=block"
     config.x_download_options = "noopen"
     config.x_permitted_cross_domain_policies = "none"
     config.referrer_policy = %w(origin-when-cross-origin strict-origin-when-cross-origin)
-    config.csp.default_src = %w('none' *.google.com)
+    config.csp.default_src = %w('none')
+    config.csp.frame_src =  %w(*.google.com)
     config.csp.base_uri = %w('self')
     config.csp.script_src = %w('self' 'unsafe-inline' *.cloudflare.com *.bootstrapcdn.com *.recaptcha.net *.gstatic.com *.googleapis.com *.google.com)
+    config.csp.style_src = %w('self' *.bootstrapcdn.com *.googleapis.com)
     config.csp.frame_ancestors = %w('none')
     config.csp.form_action = %w('self')
     config.csp.block_all_mixed_content = true
