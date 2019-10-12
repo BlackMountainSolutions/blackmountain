f07ac0115ef2215a548cfc7a3086719239bc422e
Franco Montenegro <franco.montenegro.ruke@gmail.com>
2019-10-12 03:27:27 -0300

    Add secure headers

---
+++ config/initializers/secure_headers.rb
@@ -0,0 +1,42 @@
+SecureHeaders::Configuration.default do |config|
+    config.cookies = {
+        secure: true, # mark all cookies as "Secure"
+        httponly: true, # mark all cookies as "HttpOnly"
+        samesite: {
+            lax: true # mark all cookies as SameSite=lax
+        }
+    }
+    # Add "; preload" and submit the site to hstspreload.org for best protection.
+    config.hsts = "max-age=#{1.week.to_i}"
+    config.x_frame_options = "DENY"
+    config.x_content_type_options = "nosniff"
+    config.x_xss_protection = "1; mode=block"
+    config.x_download_options = "noopen"
+    config.x_permitted_cross_domain_policies = "none"
+    config.referrer_policy = %w(origin-when-cross-origin strict-origin-when-cross-origin)
+    config.csp = {
+        # "meta" values. these will shape the header, but the values are not included in the header.
+        preserve_schemes: false, # default: false. Schemes are removed from host sources to save bytes and discourage mixed content.
+    
+        # directive values: these values will directly translate into source directives
+        default_src: %w('none'),
+        base_uri: %w('self'),
+        block_all_mixed_content: true, # see http://www.w3.org/TR/mixed-content/
+        child_src: %w('self'), # if child-src isn't supported, the value for frame-src will be set.
+        frame_src: %w('default-src' 'style-src' 'self' 'unsafe-inline' 'script-src' 'unsafe-eval' https://*),
+        connect_src: %w(wss:),
+        font_src: %w('self' data: *.gstatic.com),
+        form_action: %w('self'),
+        frame_ancestors: %w('none'),
+        img_src: %w('self' data:),
+        manifest_src: %w('self'),
+        media_src: %w('self'),
+        object_src: %w('self'),
+        sandbox: ['allow-scripts'], # true and [] will set a maximally restrictive setting
+        plugin_types: %w(application/x-shockwave-flash),
+        script_src: %w('self' 'unsafe-inline' *.cloudflare.com *.bootstrapcdn.com *.recaptcha.net *.gstatic.com *.googleapis.com *.google.com),
+        style_src: %w('self' 'unsafe-inline' *.bootstrapcdn.com *.googleapis.com),
+        worker_src: %w('self'),
+        upgrade_insecure_requests: true, # see https://www.w3.org/TR/upgrade-insecure-requests/
+    }
+end
