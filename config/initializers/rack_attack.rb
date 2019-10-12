1119d8b3f84412542fec677a349967fccb62a5e7
Franco Montenegro <franco.montenegro.ruke@gmail.com>
2019-10-12 12:24:19 -0300

    Simplify rake attack throttle

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -1,58 +1,5 @@
 class Rack::Attack
-
-    ### Configure Cache ###
-  
-    # If you don't want to use Rails.cache (Rack::Attack's default), then
-    # configure it here.
-    #
-    # Note: The store is only used for throttling (not blocklisting and
-    # safelisting). It must implement .increment and .write like
-    # ActiveSupport::Cache::Store
-  
-    # Rack::Attack.cache.store = ActiveSupport::Cache::MemoryStore.new 
-  
-    ### Throttle Spammy Clients ###
-  
-    # If any single client IP is making tons of requests, then they're
-    # probably malicious or a poorly-configured scraper. Either way, they
-    # don't deserve to hog all of the app server's CPU. Cut them off!
-    #
-    # Note: If you're serving assets through rack, those requests may be
-    # counted by rack-attack and this throttle may be activated too
-    # quickly. If so, enable the condition to exclude them from tracking.
-  
-    # Throttle all requests by IP (60rpm)
-    #
-    # Key: "rack::attack:#{Time.now.to_i/:period}:req/ip:#{req.ip}"
     throttle('req/ip', limit: 300, period: 5.minutes) do |req|
         req.ip # unless req.path.start_with?('/assets')
-    end
-    
-    ### Custom Throttle Response ###
-  
-    # By default, Rack::Attack returns an HTTP 429 for throttled responses,
-    # which is just fine.
-    #
-    # If you want to return 503 so that the attacker might be fooled into
-    # believing that they've successfully broken your app (or you just want to
-    # customize the response), then uncomment these lines.
-    # self.throttled_response = lambda do |env|
-    #  [ 503,  # status
-    #    {},   # headers
-    #    ['']] # body
-    # end
-
-    # Block suspicious requests for '/etc/password' or wordpress specific paths.
-    # After 1 blocked requests in 5 minutes, block all requests from that IP for 24 hours.
-    blocklist('fail2ban pentesters') do |req|
-        # `filter` returns truthy value if request fails, or if it's from a previously banned IP
-        # so the request is blocked
-        Rack::Attack::Fail2Ban.filter("pentesters-#{req.ip}", maxretry: 1, findtime: 5.minutes, bantime: 24.hours) do
-            # The count for the IP is incremented if the return value is truthy
-            CGI.unescape(req.query_string) =~ %r{/etc/passwd} ||
-            req.path.include?('/etc/passwd') ||
-            req.path.include?('wp-admin') ||
-            req.path.include?('wp-login')
-        end
     end
 end
