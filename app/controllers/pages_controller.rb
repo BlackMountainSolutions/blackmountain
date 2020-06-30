7ff9ebf1e30c8a953909e2da1b416e861ed4b752
Franco Montenegro <franco.montenegro.ruke@gmail.com>
2020-06-29 23:12:21 -0300

    Add mvp

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,3 @@
+class PagesController < ApplicationController
+  def what_is_an_mvp; end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :contacts, :only => [:create]
   get '/', as: 'welcome_index', to: 'welcome#index'
+  get '/what-is-an-mvp', as: 'what_is_mvp', to: 'pages#what_is_an_mvp'
   root 'welcome#index'
 end
