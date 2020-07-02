5c5bb47589de8f44a478540915c7f241e6a684fa
Franco Montenegro <franco.montenegro.ruke@gmail.com>
2020-07-01 22:52:32 -0300

    Add building an application where to begin article

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :contacts, :only => [:create]
   get '/', as: 'welcome_index', to: 'welcome#index'
   get '/what-is-an-mvp', as: 'what_is_mvp', to: 'pages#what_is_an_mvp'
+  get '/where-to-begin-when-building-an-application', as: 'building_an_application', to: 'pages#building_an_application_where_to_begin'
   root 'welcome#index'
 end
