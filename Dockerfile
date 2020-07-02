5e9b291ea0c2e3f06033f65484c9d33d55367587
Franco Montenegro <franco.montenegro.ruke@gmail.com>
2020-07-02 07:18:45 -0300

    Update to rails 6

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM ruby:2.6.4-alpine3.10
+FROM ruby:2.6.5-alpine3.10
 
 RUN apk --update add bash yarn build-base nodejs tzdata postgresql-dev postgresql-client libxslt-dev libxml2-dev imagemagick
 
--- config/initializers/secure_headers.rb
+++ config/initializers/secure_headers.rb
@@ -9,7 +9,7 @@ SecureHeaders::Configuration.default do |config|
     config.csp.frame_src =  %w(*.google.com)
     config.csp.base_uri = %w('self')
     config.csp.script_src = %w('self' 'unsafe-inline' *.fontawesome.com *.google-analytics.com *.googletagmanager.com *.cloudflare.com *.bootstrapcdn.com *.recaptcha.net *.gstatic.com *.googleapis.com *.google.com)
-    config.csp.style_src = %w('self' *.fontawesome.com *.bootstrapcdn.com *.googleapis.com)
+    config.csp.style_src = %w('self' 'unsafe-inline' *.fontawesome.com *.bootstrapcdn.com *.googleapis.com)
     config.csp.frame_ancestors = %w('none')
     config.csp.form_action = %w('self')
     config.csp.block_all_mixed_content = true
